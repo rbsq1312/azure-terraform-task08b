5659ce3bf6228465aadcf69f6164f59a6a42df84
rbsq1312 <rbsq@tuta.io>
2025-05-10 11:13:21 +0300

    infra.new code version 0.0.2

--- task08_b/modules/aca/outputs.tf
+++ task08_b/modules/aca/outputs.tf
@@ -1,19 +1,15 @@
-output "aca_fqdn" {
-  description = "The FQDN of the Azure Container App."
-  value       = azurerm_container_app.app.latest_revision_fqdn
-}
 
-output "aca_id" {
-  description = "The ID of the Azure Container App."
-  value       = azurerm_container_app.app.id
-}
-
-output "user_assigned_identity_id" {
-  description = "The ID of the User Assigned Identity created for ACA."
-  value       = azurerm_user_assigned_identity.aca_identity.id
-}
-
-output "user_assigned_identity_principal_id" {
-  description = "The Principal ID of the User Assigned Identity created for ACA."
+output "identity_principal_id" {
   value       = azurerm_user_assigned_identity.aca_identity.principal_id
+  description = "The Principal ID of the Container App's managed identity"
+}
+
+output "identity_id" {
+  value       = azurerm_user_assigned_identity.aca_identity.id
+  description = "The ID of the Container App's managed identity"
+}
+
+output "fqdn" {
+  value       = azurerm_container_app.app.latest_revision_fqdn
+  description = "The FQDN of the Container App"
 }
